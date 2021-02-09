6ee454b0d7193b13c15be9ee5b7d92073028c743
yunsuteng <75407987+Leeyunsuu@users.noreply.github.com>
2021-02-09 22:44:03 +0900

    온도변환

--- c-practice/main.c
+++ c-practice/main.c
@@ -10,6 +10,11 @@
 int main() {
     // insert code here...
   
-  
+  int num=0;
+  int tem;
+  printf("화씨온도를 입력하시오.\n");
+  scanf("%d",&num);
+  tem=(num-32)*5/9;
+  printf("변환된 섭씨온도는 %d입니다.\n", tem);
   return 0;
 }
