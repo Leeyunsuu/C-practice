458efae45f88662561f0cfbb81ed4db9edd4abae
yunsuteng <75407987+Leeyunsuu@users.noreply.github.com>
2021-02-09 23:48:05 +0900

    상수지정

--- c-practice/main.c
+++ c-practice/main.c
@@ -6,15 +6,15 @@
 //
 
 #include <stdio.h>
-
+#define EXCHANGE_RATE 1113.55
 int main() {
     // insert code here...
   
-  int num=0;
-  int tem;
-  printf("화씨온도를 입력하시오.\n");
-  scanf("%d",&num);
-  tem=(num-32)*5/9;
-  printf("변환된 섭씨온도는 %d입니다.\n", tem);
+  int num;
+  float result;
+  printf("달러를 입력하세요.\n");
+  scanf("%d", &num);
+  result= EXCHANGE_RATE*num;
+  printf("%.2f\n", result);
   return 0;
 }
