cf9457cd1a807cfe5baafbe6052286825a5cc5a0
yunsuteng <75407987+Leeyunsuu@users.noreply.github.com>
2021-02-09 17:29:30 +0900

    Initial Commit

---
+++ c-practice/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c-practice
+//
+//  Created by 이윤수 on 2021/02/09.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
