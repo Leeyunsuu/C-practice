dfb4bfe6faa88d04863b83a519c41157988ee7fd
yunsuteng <75407987+Leeyunsuu@users.noreply.github.com>
2021-02-10 23:38:22 +0900

    2021/02/10/23:34
    %08X = 8자리 16진수, 비트연산자

--- c-practice/main.c
+++ c-practice/main.c
@@ -10,19 +10,12 @@
 #define YEAR 30
 int main() {
     // insert code here...
-  int money,things,total, total2, total3;
+  int num1=9, num2=10;
   
-  printf("투입할 금액을 입력하세요.\n");
-  scanf("%d", &money);
-  printf("물건값을 입력하세요.\n");
-  scanf("%d", &things);
-  
-  total=money-things;
-  printf("거스름돈 : %d\n", total);
-  total3=total/100;
-  total2= total%100;
-  printf("거스름돈 100원의 개수 : %d\n", total3);
-  total2=total2/10;
-  printf("거스름돈 10원의 개수 : %d\n", total2);
+  printf("%08X & %08X = %08X\n",num1, num2, num1&num2);
+  printf("%08X & %08X = %08X\n",num1, num2, num1|num2);
+  printf("%08X & %08X = %08X\n",num1, num2, num1^num2);
+  printf("~%08X = %08X\n",num1, ~num1);
+
   return 0;
 }
