9efffece76339bda1a3278e18e070db30614cef9
yunsuteng <75407987+Leeyunsuu@users.noreply.github.com>
2021-02-11 17:25:35 +0900

    평면에서 두 점사이의 거리 공식

--- c-practice/main.c
+++ c-practice/main.c
@@ -7,15 +7,22 @@
 
 #include <stdio.h>
 #include <math.h>
-#define MONTH 12
-#define YEAR 30
+
 int main(void) {
     // insert code here...
-  int x=24;
-  double interest;
+  int x1, x2, y1, y2,x,y;
+  double total, total2;
   
+  printf("첫 번째 점을 입력하세요. \n");
+  scanf("%d %d", &x1, &y1);
+  printf("두 번째 점을 입력하세요. \n");
+  scanf("%d %d", &x2, &y2);
   
-  interest= x*pow(1.0+0.06, 382);
-  printf("382년 간, 24달러의 이율 : %f \n", interest);
+  x=(float)x2-(float)x1;
+  y=(float)y2-(float)y1;
+  total2=pow(x, 2)+ pow(y, 2);
+  total=sqrt(total2);
+  printf("%f입니다.\n",total);
+  printf("%f\n",total2);
   return 0;
 }
