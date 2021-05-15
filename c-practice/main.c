15f292bcb33914211622b37b6755a8dd9d19caec
yunsuteng <75407987+Leeyunsuu@users.noreply.github.com>
2021-05-16 02:05:53 +0900

    연웅이 도와준거

--- c-practice/main.c
+++ c-practice/main.c
@@ -10,30 +10,24 @@
 
 int main(void) {
     // insert code here...
-  int year, ex1, ex2, ex3;
   
-  
-  printf("희망하는 연도를 입력하세요. \n");
-  scanf("%d" , &year);
-  
-  ex1= year%4;
-  ex2= year%100;
-  ex3= year%400;
-  printf("%d\n",ex2);
-  if((ex1==0)&&(ex2==0)) {
-    if(ex3 == 0){
-      printf("%d년은 윤년입니다.\n", year);
-    } else {
-      printf("%d년은 평년입니다.\n", year);
+  int z = 1;
+  int num = -1;
+  for (int x=1; x<5; x++) {
+    for (int s=4; s>x; s--) {
+      printf("  ");
     }
-  }
-  else if (ex1 == 0 ){
-    printf("%d년은 윤년입니다.\n", year);
-  }
-  else {
-    printf("%d년은 평년입니다.\n", year);
-  }
-  
-  
-  return 0;
+    for (int y=1; y<=3*x+num; y++) {
+      if (z%2 == 0){
+        printf(" ");
+      } else{
+        printf("%d ",z);
+      }
+//      printf("*");
+      z++;
+      
+    }
+    printf("\n");
+    num++;
+  }  return 0;
 }
